f5279c0ee5cd6b78cb2c4f8d9ae2fbe7560cf504
ycechungAI <ycechung@myseneca.ca>
2021-11-15 17:08:19 -0500

    Submitting lab9 * base code v1 by fixing existing code
    * finished 2 sets of tests for Jest
    
    * added to CONTRIBUTION.md

---
+++ test/helper.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const { isFileCheck } = require("./../bin/helper");
+
+describe("IsFileCheck check", () => {
+  it("Check for correct file", () => {
+    const response = isFileCheck("..\\sample_txt\\The Naval Treaty.txt");
+    expect(response).toBe(true);
+  });
+
+  it("Check for directory", () => {
+    const response = isFileCheck("..\\sample_txt\\");
+    expect(response).toBe(false);
+  });
+
+  it("Check for not .txt or .md files", () => {
+    const response = isFileCheck("./index.html");
+    expect(response).toBe(false);
+  });
+});
+
+//INPUT CHECKS
+const { checkInput } = require("./../bin/helper");
+describe("Input argv check", () => {
+  it("Check for correct input file", () => {
+    try {
+      checkInput("README.md");
+      expect(true).toBe(true);
+    } catch (error) {
+      expect(error).toBe(false);
+    }
+  });
+
+  it("Check for non exist file", () => {
+    try {
+      checkInput("holyghost.txt");
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe("Directory or file must exist.");
+    }
+  });
+});
