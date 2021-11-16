67775b012d03c110e0d49317873e525fd6bd2007
ycechungAI <ycechung@myseneca.ca>
2021-11-15 19:32:06 -0500

    finished 2 sets of tests for Jest

--- test/helper.test.js
+++ test/helper.test.js
@@ -1,18 +1,42 @@
-const { helper } = require("./../bin/helper");
+/* eslint-disable no-undef */
+const fs = require("fs");
+const { isFileCheck } = require("./../bin/helper");
 
 describe("IsFileCheck check", () => {
   it("Check for correct file", () => {
-    const response = helper.isFileCheck("src\\sample_txt\\Silver Surfer.txt");
+    const response = isFileCheck("..\\sample_txt\\The Naval Treaty.txt");
     expect(response).toBe(true);
   });
 
   it("Check for directory", () => {
-    const response = helper.isFileCheck("src\\sample_txt\\");
+    const response = isFileCheck("..\\sample_txt\\");
     expect(response).toBe(false);
   });
 
   it("Check for not .txt or .md files", () => {
-    const response = helper.isFileCheck("src\\.gitignore");
+    const response = isFileCheck("./index.html");
     expect(response).toBe(false);
+  });
+});
+
+//INPUT CHECKS
+const { checkInput } = require("./../bin/helper");
+describe("Input argv check", () => {
+  it("Check for correct input file", () => {
+    try {
+      checkInput("README.md");
+      expect(true).toBe(true);
+    } catch (error) {
+      expect(error).toBe(false);
+    }
+  });
+
+  it("Check for non exist file", () => {
+    try {
+      checkInput("holyghost.txt");
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toBe("Directory or file must exist.");
+    }
   });
 });
